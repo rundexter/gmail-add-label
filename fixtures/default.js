f748250d9be3e08c784ddd38e4f7c45de4e8007b
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-21 13:01:41 +0200

    fix Oauth

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,9 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                id: '1525faa8a5700d38',
+                userId: 'dexter.test@gmail.com',
+                removeLabelIds: ['CATEGORY_PROMOTIONS']
             }
         }
     }
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var _ = require('lodash'),
-  google = require('googleapis'),
-  OAuth2 = google.auth.OAuth2;
+    util = require('./util.js'),
+    google = require('googleapis'),
+    service = google.gmail('v1');
+
+var pickInputs = {
+        'id': { key: 'id', validate: { req: true } },
+        'userId': { key: 'userId', validate: { req: true } },
+        'addLabelIds': { key: 'resource.addLabelIds', type: 'array' },
+        'removeLabelIds': { key: 'resource.removeLabelIds', type: 'array' }
+    },
+    pickOutputs = {
+        'id': 'id',
+        'threadId': 'threadId',
+        'labelIds': 'labelIds'
+    };
 
 var requireParams = ['id', 'userId'];
-var pickReqAttr = [
-    'id',
-    'threadId',
-    'labelIds',
-    'snippet',
-    'historyId',
-    'internalDate',
-    'payload',
-    'body',
-    'parts'
-];
 
 module.exports = {
     checkAuthOptions: function (step, dexter) {
@@ -37,23 +39,24 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var OAuth2 = google.auth.OAuth2,
+            oauth2Client = new OAuth2(),
+            credentials = dexter.provider('google').credentials();
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        this.checkAuthOptions(step, dexter);
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-        var oauth2Client = new OAuth2();
-        oauth2Client.setCredentials({access_token: dexter.environment('google_access_token'), refresh_token: dexter.environment('google_refresh_token')});
-
+        // set credential
+        oauth2Client.setCredentials({
+            access_token: _.get(credentials, 'access_token')
+        });
         google.options({ auth: oauth2Client });
-        google.gmail('v1').users.messages.modify({
-            auth: oauth2Client,
-            id: step.input('id', null).first(),
-            userId: step.input('userId', null).first(),
-            resource: {
-                addLabelIds: step.input('addLabelIds').toArray(),
-                removeLabelIds: step.input('removeLabelIds').toArray()
-            }}, function (err, message) {
 
-            err? this.fail(err) : this.complete(_.pick(message, pickReqAttr));
+        service.users.messages.modify(inputs, function (err, message) {
+
+            err? this.fail(err) : this.complete(util.pickOutputs(message, pickOutputs));
         }.bind(this));
 
     }
